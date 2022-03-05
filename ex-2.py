e6e86e42ff2bcf4c90ae232eb409ac97f73887c7
Jbrfm <mobin.jabbari4752@gmail.com>
2022-03-06 00:17:58 +0330

    first comit for second tamrin

---
+++ ex-2.py
@@ -0,0 +1,25 @@
+import json
+
+users = []
+
+def getUser():
+    name = input("Enter name : ")
+    age = int(input("Enter age : "))
+    return { "name": name, "age": age }
+
+def searchUsers(searchedName: str): 
+    for item in users:
+        if(item['name'] == searchedName):
+            return item['age']
+    return 'User does not exist!!'
+
+def main():
+    userCount = int(input("Enter user Count : "))
+    
+    for i in range(0, userCount):
+        users.append(getUser())
+
+    searchedName = input('Enter the name : ')
+    print(searchUsers(searchedName))
+
+main()
